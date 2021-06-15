4c249ef84a2e8aa056cb08b2e77642180a3b0793
Maxim Palotti <41835828+MyHouseIsBig@users.noreply.github.com>
2021-06-15 17:50:14 +0200

    Add files via upload

---
+++ TestConsoleAppForTNT/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Xml;
+using TNT_ClassLibrary;
+
+namespace TestConsoleAppForTNT
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Richiesta nuovaRichiesta = new();
+                XmlDocument risposta = await Richiesta.NuovaRichiestaXml("<data><totalpackages>1</totalpackages><goodsdesc>VARI</goodsdesc></data>");
+
+                Console.WriteLine();
+                if (risposta.SelectSingleNode("/Label/Incomplete/Message") != null) 
+                {
+                    throw new Exception(risposta.SelectSingleNode("/Label/Incomplete/Message").InnerText);
+                } else
+                {
+                    Console.WriteLine(risposta.ToString());
+                }
+            }
+            catch (Exception aex)
+            {
+                Console.WriteLine($"Exception: {aex.Message}");
+            }
+        }
+    }
+}
